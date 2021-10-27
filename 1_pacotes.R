7ffd3e2f386583c0fdf777362c10498b9a31f750
Lidiany <lidianycs@gmail.com>
2021-10-27 15:07:55 -0300

    apagar config

--- 1_pacotes.R
+++ 1_pacotes.R
@@ -5,15 +5,5 @@ install.packages("RSQLite")
   install.packages("data.table")
   install.packages("plyr")
   
-  #p/ config git
-  install.packages("usethis")
-  library(usethis)
-  
-  usethis::use_git_config(user.name = "Lidiany", # Seu nome
-                          user.email = "lidianycs@gmail.com") # Seu email
-  
-  usethis::create_github_token()
-  usethis::edit_r_environ()  
-  
-  usethis::use_git()
+
   
